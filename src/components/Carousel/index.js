f430390e8dd97d7de28000a616582804b476dd73
tomlowen <tom.r.lowen@googlemail.com>
2021-10-12 21:42:52 +0100

    image resize sorted

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -3,18 +3,22 @@ import image2 from "../../images/2.png";
 
 export function ControlledCarousel() {
   return (
-    <div className="top-10">
-      <div className="relative w-screen min-h-screen pb-2/3">
+    <div className="absolute top-20">
+      <div className="absolute w-screen min-h-screen pb-2/3">
         <img
           className="w-full h-full absolute z-20 object-cover"
           src={image1}
           alt="Tom and Frankie in 2010"
         />
         <img
-          className="w-full h-full absolute z-10 animate-fade object-cover"
+          className="w-full h-full absolute z-10 object-cover"
           src={image2}
           alt="Tom and Frankie in 2021"
         />
+      </div>
+      <div>
+        <p>After 12 years</p>
+        <p>We're getting married!</p>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
         "1/3": "33.33333%",
         "2/3": "66.66667%",
       },
+      animation: {
+        fade: "fadeOut 3s ease-in-out",
+      },
+
+      keyframes: (theme) => ({
+        fadeOut: {
+          "0%": { backgroundColor: theme("colors.red.300") },
+          "100%": { backgroundColor: theme("colors.transparent") },
+        },
+      }),
     },
     variants: {
       extend: {},
